1c5cbd84f0f476758b3eaa706882dba4ed570ad8
Kartik Shankhavaram <shankhavaramk@gmail.com>
2017-11-01 18:31:20 +0530

    Added helper comments

--- app/src/main/java/xyz/cybros/magic8ball/MainActivity.java
+++ app/src/main/java/xyz/cybros/magic8ball/MainActivity.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Add declarations for objects(Button, TextView, EditText, etc.)
+     * and primitive variables(int, float, etc.) here
+     * e.g.
+     * TextView textView;
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Initialize the variables and objects here
+         * e.g.
+         * textView = (TextView) findViewById(R.id.textView);
+         * (set the id as textView in activity_main.xml)
+         */
     }
 }
